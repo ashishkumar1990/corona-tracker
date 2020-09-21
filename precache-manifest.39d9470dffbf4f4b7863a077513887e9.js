935e5cc5c18223df96b0079f9b011d4cb124af15
Ashish kumar <akumar@okruti.com>
2020-09-21 10:29:00 +0530

    Updates

---
+++ precache-manifest.39d9470dffbf4f4b7863a077513887e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ce0404ab2570807a826a252c882ac1",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "652bfd55b62da811e861",
+    "url": "/corona-tracker/static/css/main.c85cec27.chunk.css"
+  },
+  {
+    "revision": "9c90d1de45d3a9b53618",
+    "url": "/corona-tracker/static/js/2.97028599.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/corona-tracker/static/js/2.97028599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652bfd55b62da811e861",
+    "url": "/corona-tracker/static/js/main.b015650c.chunk.js"
+  },
+  {
+    "revision": "9c4d7b1cf1de7ae99f79",
+    "url": "/corona-tracker/static/js/runtime-main.bf840863.js"
+  },
+  {
+    "revision": "485a55b2446cc2d56028db0b29c0cf1c",
+    "url": "/corona-tracker/static/media/Coronavirus.485a55b2.gif"
+  },
+  {
+    "revision": "b84807139d49397753676fb8c603fc67",
+    "url": "/corona-tracker/static/media/covid-19.b8480713.jpg"
+  },
+  {
+    "revision": "fcd5c19212b0a8900fe3be0fcaa741c7",
+    "url": "/corona-tracker/static/media/heart.fcd5c192.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52a31ea1bc704ad35b57e38121f450ba.js"
+  "/corona-tracker/precache-manifest.39d9470dffbf4f4b7863a077513887e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/corona-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
