e65da7914dd3f45db6ad434113cfcb2b02253579
Ashish kumar <akumar@okruti.com>
2020-09-21 10:26:16 +0530

    Updates

---
+++ precache-manifest.52a31ea1bc704ad35b57e38121f450ba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8dc1cf7f4ca20a98494ef752c1247be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "711aa6ccc2049cf413b2",
+    "url": "/static/css/main.c85cec27.chunk.css"
+  },
+  {
+    "revision": "bdba12056f49b85b4141",
+    "url": "/static/js/2.3ba23b72.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/static/js/2.3ba23b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "711aa6ccc2049cf413b2",
+    "url": "/static/js/main.8e2e7ca9.chunk.js"
+  },
+  {
+    "revision": "717a8abd0f249375b28a",
+    "url": "/static/js/runtime-main.0dea4190.js"
+  },
+  {
+    "revision": "485a55b2446cc2d56028db0b29c0cf1c",
+    "url": "/static/media/Coronavirus.485a55b2.gif"
+  },
+  {
+    "revision": "b84807139d49397753676fb8c603fc67",
+    "url": "/static/media/covid-19.b8480713.jpg"
+  },
+  {
+    "revision": "fcd5c19212b0a8900fe3be0fcaa741c7",
+    "url": "/static/media/heart.fcd5c192.jpg"
+  }
+]);
